b99d791df41d8bd71cc5e030184d7a5cd2bafe6d
Luke Lynch <Tech.Luk3@gmail.com>
2022-09-03 15:48:11 +1000

    Added dependencies. moved forwards.js to src folder. added comments

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import lnService from 'ln-service';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 dotenv.config();
-import { CalculateTotalFeesEarned, CalculateTotalForwardAmount } from './forwards.js'
+import { CalculateTotalFeesEarned, CalculateTotalForwardAmount } from './src/forwards.js';
+import { getChannelAge } from './src/channels.js';
 
 const { lnd } = lnService.authenticatedLndGrpc({
     cert: process.env.CERT,
@@ -11,7 +12,7 @@ const { lnd } = lnService.authenticatedLndGrpc({
 
 const { channels } = (await lnService.getChannels({ lnd }));
 
-const afterDate = new Date('31 August 2022 00:00 GMT+10').toISOString();
+const afterDate = new Date('1 August 2022 00:00 GMT+10').toISOString();
 const beforeDate = new Date('31 August 2022 23:59 GMT+10').toISOString();
 
 const forwards = (await lnService.getForwards({ after: afterDate, before: beforeDate, lnd: lnd, limit: 1000 })).forwards.reverse();
@@ -28,17 +29,24 @@ for (let i = 0; i < channels.length; i++) {
 
 let totalFeesEarned = 0;
 let totalOutgoing = 0;
+// Calculate total fees and forwarding amoutn
 for (let i = 0; i < channels.length; i++) {
     channels[i].feesEarned = CalculateTotalFeesEarned(channels[i].outgoing_forwards);
     channels[i].totalForwarded = CalculateTotalForwardAmount(channels[i].outgoing_forwards);
     totalFeesEarned += channels[i].feesEarned;
     totalOutgoing += channels[i].outgoing_forwards.length;
-    // console.log(`${channels[i].partner_alias}:
-    // Incoming Forwards: ${channels[i].incoming_forwards.length}
-    // Outgoing Forwards: ${channels[i].outgoing_forwards.length}
-    // Total Amount Forwarded: ${channels[i].totalForwarded.toLocaleString()} sats
-    // Total Fees Earned: ${channels[i].feesEarned.toLocaleString()} sats`);
 };
+
+// Sort channels from least fees earned to most fees earned
+channels.sort(function (a, b) {return a.feesEarned - b.feesEarned});
+
+for (let i = 0; i < channels.length; i++) {
+    console.log(`${channels[i].partner_alias}:
+    Incoming Forwards: ${channels[i].incoming_forwards.length}
+    Outgoing Forwards: ${channels[i].outgoing_forwards.length}
+    Total Amount Forwarded: ${channels[i].totalForwarded.toLocaleString()} sats
+    Total Fees Earned: ${channels[i].feesEarned.toLocaleString()} sats`);
+}
 
 console.log(`Total Fees Earned: ${totalFeesEarned} sats`);
 console.log(`Total Forwards: ${totalOutgoing}`);
